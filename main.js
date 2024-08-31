c54ea5867e8cebfba6d45ad3cb9d269939ccebc6
Mohamed Nour <132625813+midonour@users.noreply.github.com>
2024-08-31 22:46:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const login=document.getElementById('log');
+login.addEventListener('click',()=>{
+    window.location.href="bookreader_login.html";
+})
+
+const signup=document.getElementById('sign');
+signup.addEventListener('click',()=>{
+    window.location.href="bookreader_signup.html";
+})
